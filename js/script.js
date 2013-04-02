a463716319e49b959cba03cd91f3b80f9bb69f06
Aero Windwalker <aero.windwalker@gmail.com>
2013-04-01 23:34:53 -0400

    init

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// check for Geolocation support
+if (navigator.geolocation) {
+  console.log('Geolocation is supported!');
+}
+else {
+  console.log('Geolocation is not supported for this Browser/OS version yet.');
+}
