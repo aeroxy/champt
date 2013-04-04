c772328db1b392354efc55916515c1088a5ec83e
Aero Windwalker <aero.windwalker@gmail.com>
2013-04-04 06:25:19 -0400

    cannot label text

--- js/script.js
+++ js/script.js
@@ -1,7 +1,35 @@
-// check for Geolocation support
-if (navigator.geolocation) {
-  console.log('Geolocation is supported!');
-}
-else {
-  console.log('Geolocation is not supported for this Browser/OS version yet.');
-}
+window.onload = function() {
+	function drawMe() {
+		navigator.geolocation.watchPosition(function(position) {
+			var mapOptions = {
+				center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+				zoom: 18,
+				mapTypeId: google.maps.MapTypeId.ROADMAP,
+				draggable: false,
+				zoomControl: false,
+				scrollwheel: false,
+				disableDoubleClickZoom: true,
+				disableDefaultUI: true,
+			};
+			var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+			var yourLocation = {
+				strokeColor: "#FF0000",
+				strokeOpacity: 0.5,
+				strokeWeight: 1,
+				fillColor: "#FF0000",
+				fillOpacity: 1,
+				map: map,
+				center: new google.maps.LatLng(40.7474553, -73.9879989),
+				radius: 4,
+			};
+			var drawCircle = new google.maps.Circle(yourLocation);
+		}, function(error) {
+			alert('Error occurred. Error code: ' + error.code);
+		});
+	}
+	drawMe();
+};
+
+$(window).on("resize", function(){
+	drawMe();
+});
