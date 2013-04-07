f67acb2e9e5cba001784646ebb790817cbb9b30d
Aero Windwalker <aero.windwalker@gmail.com>
2013-04-06 20:21:40 -0400

    bubble

--- www/js/script.js
+++ www/js/script.js
@@ -43,6 +43,52 @@ $(document).ready(function() {
 			google.maps.event.addDomListener(changeUserName, 'click', function() {
 				mapUserName.set('text', document.getElementById('userName').value);
 			});
+
+//fuck
+			// var contentString = '<div id="content">'+
+			// '<div id="siteNotice">'+
+			// '</div>'+
+			// '<h2 id="firstHeading" class="firstHeading">Uluru</h2>'+
+			// '<div id="bodyContent">'+
+			// '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+			// 'sandstone rock formation in the southern part of the '+
+			// 'Northern Territory, central Australia. It lies 335 km (208 mi) '+
+			// 'south west of the nearest large town, Alice Springs; 450 km '+
+			// '(280 mi) by road. Kata Tjuta and Uluru are the two major '+
+			// 'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+			// 'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+			// 'Aboriginal people of the area. It has many springs, waterholes, '+
+			// 'rock caves and ancient paintings. Uluru is listed as a World '+
+			// 'Heritage Site.</p>'+
+			// '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+			// 'http://en.wikipedia.org/w/index.php?title=Uluru</a> (last visited June 22, 2009).</p>'+
+			// '</div>'+
+			// '</div>';
+
+			var infoBubble = new InfoBubble({
+				content: '<div><p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+			'sandstone rock formation in the southern part of the '+
+			'Northern Territory, central Australia. It lies 335 km (208 mi) '+
+			'south west of the nearest large town, Alice Springs; 450 km '+
+			'(280 mi) by road. Kata Tjuta and Uluru are the two major '+
+			'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+			'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+			'Aboriginal people of the area. It has many springs, waterholes, '+
+			'rock caves and ancient paintings. Uluru is listed as a World '+
+			'Heritage Site.</p></div>',
+				// position: gLatLng,
+				maxWidth: 300
+			});
+
+			infoBubble.open(map, yourLocation);
+
+			// google.maps.event.addListener(yourLocation, 'click', function() {
+			// 	if (!infoBubble.isOpen()) {
+			// 		infoBubble.open(map, yourLocation);
+			// 	}
+			// });
+
+//fuck
 		}, function(error) {
 			alert('Error occurred. Error code: ' + error.code);
 		});
