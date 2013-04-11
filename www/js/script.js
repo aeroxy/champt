f18df3b9b079cb7e4e0e98aed7bec24c9b7583d2
Aero Windwalker <aero.windwalker@gmail.com>
2013-04-11 17:30:16 -0400

    socket improved

--- www/js/script.js
+++ www/js/script.js
@@ -1,77 +1,114 @@
 var socket = io.connect('http://localhost');
 $(document).ready(function(){
-	function drawMap(latitude,longitude){
-		var gLatLng = new google.maps.LatLng(latitude,longitude);
-		var mapOptions = {
-			center: gLatLng,
-			zoom: 18,
-			mapTypeId: google.maps.MapTypeId.ROADMAP,
-			draggable: false,
-			zoomControl: false,
-			scrollwheel: false,
-			disableDoubleClickZoom: true,
-			disableDefaultUI: true,
-		};
-		var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
-		var nameLatLng = new google.maps.LatLng(latitude - 0.00004, longitude);
-		var userLocation = {
-			path: google.maps.SymbolPath.CIRCLE,
-			strokeColor: "#4c8f2a",
-			strokeWeight: 2,
-			fillColor: "#a2e346",
-			fillOpacity: 1,
-			scale: 6,
-		};
-		var drawCircle = new google.maps.Marker({
-			position: gLatLng,
-			icon: userLocation,
-			map: map,
-		});
-		var mapUserName = new MapLabel({
-			text: 'Anonymous User',
-			position: nameLatLng,
-			map: map,
-			fontSize: 14,
-			fontColor: "#ffffff",
-			strokeWeight: 4,
-			strokeColor: "4c8f2a",
-			fontFamily: "Avenir",
+	function resizeText() {
+		var windowHeight = $(window).height();
+		var windowWidth = $(window).width();
+		$("footer").css({
+			'padding-top': windowHeight * 0.15 * 0.1 + 'px',
 		});
 	};
-	function drawUser(latitude,longitude){
-		var gLatLng = new google.maps.LatLng(latitude,longitude);
-		var nameLatLng = new google.maps.LatLng(latitude - 0.00004, longitude);
-		var userLocation = {
-			path: google.maps.SymbolPath.CIRCLE,
-			strokeColor: "#4c8f2a",
-			strokeWeight: 2,
-			fillColor: "#a2e346",
-			fillOpacity: 1,
-			scale: 6,
-		};
-		var drawCircle = new google.maps.Marker({
-			position: gLatLng,
-			icon: userLocation,
-			map: map,
-		});
-		var mapUserName = new MapLabel({
-			text: 'Anonymous User',
-			position: nameLatLng,
-			map: map,
-			fontSize: 14,
-			fontColor: "#ffffff",
-			strokeWeight: 4,
-			strokeColor: "4c8f2a",
-			fontFamily: "Avenir",
-		});
-	};
-	var drawEverything = navigator.geolocation.watchPosition(function(position){
+	resizeText();
+	$(window).resize(function(){
+		resizeText();
+	});
+	navigator.geolocation.watchPosition(function(position){
 		var latitude = position.coords.latitude;
 		var longitude = position.coords.longitude;
+		console.log(latitude,longitude);
 		socket.emit('addToMap',latitude,longitude);
 		socket.on('meOnMap',function(latitude,longitude){
-			drawMap(latitude,longitude);
+			var gLatLng = new google.maps.LatLng(latitude, longitude);
+			function drawMap(){
+				var mapOptions = {
+					center: gLatLng,
+					zoom: 18,
+					mapTypeId: google.maps.MapTypeId.ROADMAP,
+					draggable: false,
+					zoomControl: false,
+					scrollwheel: false,
+					disableDoubleClickZoom: true,
+					disableDefaultUI: true,
+				};
+				var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+				return map;
+			}
+			var map = drawMap();
+			function drawUser(){
+				var nameLatLng = new google.maps.LatLng(latitude - 0.00004, longitude);
+				var userLocation = {
+					path: google.maps.SymbolPath.CIRCLE,
+					strokeColor: "#4c8f2a",
+					strokeWeight: 2,
+					fillColor: "#a2e346",
+					fillOpacity: 1,
+					scale: 6,
+				};
+				var userName = new MapLabel({
+					text: 'Anonymous User',
+					position: nameLatLng,
+					map: map,
+					fontSize: 14,
+					fontColor: "#ffffff",
+					strokeWeight: 4,
+					strokeColor: "4c8f2a",
+					fontFamily: "Avenir",
+				});
+				var drawCircle = new google.maps.Marker({
+					position: gLatLng,
+					icon: userLocation,
+					map: map,
+				});
+				return userName;
+			};
+			var userName = drawUser();
+			function updateUser(){
+				var infoBubble = new InfoBubble({
+					position: gLatLng,
+					maxWidth: 300,
+					backgroundColor: 'rgba(255,255,255,0.8)',
+					backgroundClassName: 'bubble',
+					hideCloseButton: true,
+					shadowStyle: 0,
+				});
+				$("#update").click(function(){
+					var status = $("#status-update").val();
+					userName.set('text', $("#userName").val());
+					infoBubble.addTab($("#userName").val() + ':', status);
+					infoBubble.open(map);
+				});
+			};
+			updateUser();
 			socket.on('otherOnMap',function(latitude,longitude){
+				var gLatLng = new google.maps.LatLng(latitude, longitude);
+				function drawOtherUsers(){
+					var nameLatLng = new google.maps.LatLng(latitude - 0.00004, longitude);
+					var userLocation = {
+						path: google.maps.SymbolPath.CIRCLE,
+						strokeColor: "#ff0000",
+						strokeWeight: 2,
+						fillColor: "#ffffff",
+						fillOpacity: 1,
+						scale: 6,
+					};
+					var userName = new MapLabel({
+						text: 'Anonymous User',
+						position: nameLatLng,
+						map: map,
+						fontSize: 14,
+						fontColor: "#ffffff",
+						strokeWeight: 4,
+						strokeColor: "#ff0000",
+						fontFamily: "Avenir",
+					});
+					var drawCircle = new google.maps.Marker({
+						position: gLatLng,
+						icon: userLocation,
+						map: map,
+					});
+					return userName;
+				};
+				var userName = drawOtherUsers();
+				updateUser();
 			});
 		});
 	});
